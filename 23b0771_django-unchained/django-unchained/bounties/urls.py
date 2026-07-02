985e0198e8c8659403004e6c52bf235b1c033968
Prakhar <prakharg879@gmail.com>
2026-07-02 22:44:41 +0530

    Delete 23b0771_django-unchained/django-unchained directory

--- 23b0771_django-unchained/django-unchained/bounties/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import BountyListCreateView, BountyDetailView
-
-urlpatterns = [
-    path('bounties/', BountyListCreateView.as_view(), name='bounty-list-create'),
-    path('bounties/<int:pk>/', BountyDetailView.as_view(), name='bounty-detail'),
-]
